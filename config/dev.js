c93fa30a165101818698e2d038e6cda5025cb9ba
MohammadMahdi <mamiriesfahani@gmail.com>
2021-06-11 23:43:48 +0430

    change git ignore

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI : "mongodb+srv://mohammadmahdi:UsrLtmlVz4vdaen3@cluster0.5q3z1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET: "effffefefefefefefefef"
-}
--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI : process.env.MONGO_DB || "mongodb+srv://mohammadmahdi:UsrLtmlVz4vdaen3@cluster0.5q3z1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:  process.env.SECRET || "effffefefefefefefefef"
-}
